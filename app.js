bc4c99112ca16ba48fa82346bc13120ed03d02a7
fahadkhan917 <fk16181@gmail.com>
2020-07-15 07:36:52 +1000

    updated

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
-function abc(){
+// function abc(){
 
-var a=document.getElementById("im1");
-a.src="contact usbg.jpg"
+// var a=document.getElementById("im1");
+// a.src="contact usbg.jpg"
     
+// }
+
+// function xyz(){
+
+//     var b=document.getElementById("im1");
+//     b.src="branding.jpg"
+        
+//     }
+var count=0;
+function inc(){
+  
+++count;
+var a=document.getElementById("para");
+para.innerText=count;
+
+
 }
 
-function xyz(){
-
-    var b=document.getElementById("im1");
-    b.src="branding.jpg"
-        
+function dec(){
+  
+    count--;
+    var b=document.getElementById("para");
+    para.innerText=count;
+    
+    
     }
