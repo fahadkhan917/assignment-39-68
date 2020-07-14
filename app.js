ccddcd75e8d86190c456cb7aa194103b1ae46719
fahadkhan917 <fk16181@gmail.com>
2020-07-14 20:13:32 +1000

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+function abc(){
+
+var a=document.getElementById("im1");
+a.src="contact usbg.jpg"
+    
+}
+
+function xyz(){
+
+    var b=document.getElementById("im1");
+    b.src="branding.jpg"
+        
+    }
