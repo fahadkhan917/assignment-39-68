ad7037788afae3d597f100777b30df3b3aef5a65
fahadkhan917 <fk16181@gmail.com>
2020-07-16 05:45:22 +1000

    updated

--- app.js
+++ app.js
@@ -30,14 +30,22 @@
     
 //     }
 
+// function abc(){
+
+// var user=document.getElementById("usr").value;
+// var email=document.getElementById("email").value;
+// var pass=document.getElementById("pass").value;
+// var conpass=document.getElementById("conpass").value;
+
+// document.write(user+"<br>"+email+"<br>"+pass+"<BR>"+conpass);
+
+// }
+
+
 function abc(){
 
-var user=document.getElementById("usr").value;
-var email=document.getElementById("email").value;
-var pass=document.getElementById("pass").value;
-var conpass=document.getElementById("conpass").value;
-
-document.write(user+"<br>"+email+"<br>"+pass+"<BR>"+conpass);
-
+var para="Neque, iste numquam culpa rerum odio rem aliquam voluptatem sunt? Hic illum, atque quae aspernatur accusantium explicabo possimus assumenda sapiente laudantium voluptatem!"
+var pr=document.getElementById("para1");
+pr.innerHTML+=para;
 
 }
